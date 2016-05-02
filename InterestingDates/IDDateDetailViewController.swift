133d36fa7f1c90887aa049a9cf97e16988043889
Rommel Rico <rommeltj@gmail.com>
2016-05-02 10:57:06 -0700

    UI and Auto Layout

--- InterestingDates/IDDateDetailViewController.swift
+++ InterestingDates/IDDateDetailViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class IDDateDetailViewController: UIViewController {
+    @IBOutlet weak var dateTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
